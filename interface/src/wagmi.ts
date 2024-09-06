60012c51f674e7812caf0d833d2ff4f2f5146b72
Piotr Ostrowski <piotr.jp.ostrowski@gmail.com>
2024-09-06 20:52:14 +0200

    wagmi setup

---
+++ interface/src/wagmi.ts
@@ -0,0 +1,21 @@
+import { http, createConfig } from "wagmi";
+import { optimism } from "wagmi/chains";
+import { coinbaseWallet, injected, walletConnect } from "wagmi/connectors";
+
+export const config = createConfig({
+  chains: [optimism],
+  connectors: [
+    injected(),
+    coinbaseWallet(),
+    walletConnect({ projectId: import.meta.env.VITE_WC_PROJECT_ID }),
+  ],
+  transports: {
+    [optimism.id]: http(),
+  },
+});
+
+declare module "wagmi" {
+  interface Register {
+    config: typeof config;
+  }
+}
